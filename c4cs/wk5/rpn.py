7332d113c78471cd77e42de4b6338f531fb68a0f
Spencer Maroukis <smarouki@umich.edu>
2016-10-05 14:38:18 -0400

    lotsa stuff

--- c4cs/wk5/rpn.py
+++ c4cs/wk5/rpn.py
@@ -1,21 +1,28 @@
 #!/Users/Spencer/Distributions_et_al/anaconda/envs/pandahacks/bin python
 
 def calculate(myarg):
-	stack - list()
+	stack = list()
 	for token in myarg.split():
 		if token == '+':
 			arg1 = stack.pop()
 			arg2 = stack.pop()
 			result = arg1 + arg2
 			stack.append(result)
+		elif token == '-':
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = arg1 - arg2
+			stack.append(result)
 		else:
 			stack.append(int(token))
 		print(stack)
+	if len(stack) != 1:
+			raise TypeError("Too many parameters")
 	return stack.pop()
 
 def main():
 	while True:
-		calculate(input("rpn calc> "))
+		calculate(raw_input("rpn calc> "))
 
 if __name__=="__main__":
 	main()	
--- c4cs/wk5/test_rpn.py
+++ c4cs/wk5/test_rpn.py
@@ -5,3 +5,16 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
+	def test_subtract(self):
+		result = rpn.calcualte("3 5 -")
+		self.assertEqual(2, result)
+	def test_badstring(self):
+		with self.assertRaises(TypeError):
+			rpn.calcuate("1 2 3 +")
+	def test_multiply(self):
+		result = rpn.calculate("2 3 *")
+		self.assertEqual(6, result)
+	def test_divide(self):
+		result = rpn.calculate("10 5 /")
+		self.assertEqual(2, result)
+	
